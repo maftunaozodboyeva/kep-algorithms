e3b874ac9a0edd41971b4333be6d3a6d9bb5de0f
maftunaozodboyeva86@gmail.com <maftunaozodboyeva86>
2026-04-25 17:46:47 +0500

    Add new files for prime number checking and user interaction, and update existing files for consistency

---
+++ 11.py
@@ -0,0 +1,9 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    for son in range(2, (n**0.5) + 1):
+        if n % son == 0:
+            return False
+    return True
+n = int(input())
+print("Yes") if is_prime(n) else print("No")
---
+++ 2294.py
@@ -0,0 +1,2 @@
+a = input("Yoqtirgan soningni kirit: ")
+print(f"{a} seni jinni ekanligingni aytdi😁🤣")
